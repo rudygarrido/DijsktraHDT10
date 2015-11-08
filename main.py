a1ed6fd55aebeda7bf3690c95897437005b7c6fd
DelbertCV <dfcustodiovargas@gmail.com>
2015-11-08 09:59:36 -0600

    Documentacion

--- main.py
+++ main.py
@@ -2,13 +2,20 @@ import networkx as nx
 G=nx.DiGraph()
 file = open('ciudades.txt', 'r')
 
+
+"""
+Se lee cada linea del archivo de texto, y seguidamente se
+usa el espacio entre cada palabra como el delimitador
+"""
 for line in file:
     words = line.split(" ")
+    w2 = len(words[2])
+    words[2] = words[2][:w2]
     G.add_edge(words[0], words[1], weight=int(words[2]))
 
 print 'Bienvenido al manejador de ciudades'
 
-
+#Menu principal
 while 1:
     print ''
     print '1. Mostrar la matriz de adyacencia'
@@ -21,12 +28,14 @@ while 1:
     except ValueError:
             print "Opcion no valida"
     if opcion==1:
+        #Se llama a la funcion nodes() para obtener los nodos del grafo
         print 'Matriz de Adyacencia'
         nodos = G.nodes()
         encabezado = ''
         for nodo in nodos:
             encabezado = encabezado+nodo+'\t'
         print '\t'+encabezado
+        #Impresion de nodos
         for nodoActual in nodos:
             linea = ''
             linea = linea + nodoActual + '\t'
@@ -41,19 +50,23 @@ while 1:
         ciudadInicial = raw_input('Ciudad inicial: ')
         ciudadFinal = raw_input('Ciudad final: ')
         try:
+            #Se calcula el camino mas corto
             camino = nx.shortest_path(G, source=ciudadInicial, target=ciudadFinal)
             print 'Las ciudades por las que debe pasar son: '   
+            #Impresion de la lista conteniendo el camino a seguir            
             print camino
             peso = 0
             posicion =0;
             for x in range(0, len(camino)-1):
-                peso = peso + G[camino[x]][camino[x+1]]['weight']
-            print 'Con el peso'+peso
+                #G[x][y]['weight'] es el peso de la conexion o edge
+                peso = peso +  G[camino[x]][camino[x+1]]['weight']
+            print 'Con el peso '+peso
         except:
             print 'No hay camino entre estas ciudades'
             
 
     if opcion ==3:
+        #Eliminacion de una conexion entre nodos
         ciudadInicial = raw_input('Ciudad inicial: ')
         ciudadFinal = raw_input('Ciudad final: ')
         try:
@@ -61,6 +74,9 @@ while 1:
         except:
              print 'No hay camino entre estas ciudades'   
     if opcion==4:
+        #Adicion de una conexion entre nodos
+    
+        #Sources o nodos a enlazar
         ciudadInicial = raw_input('Ciudad inicial: ')
         ciudadFinal = raw_input('Ciudad final: ')
         try:
